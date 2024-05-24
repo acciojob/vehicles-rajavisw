a63a48b4590a03d0c246ecfe795fc882d30bbf46
rajavisw <rajaviswanadhapalli12@gmail.com>
2024-05-24 10:07:55 +0530

    coded successfully

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -1,2 +1,21 @@
-package com.driver;public class Boat {
+package com.driver;
+
+public class Boat implements WaterVehicle{
+    private String name;
+    private int capacity;
+
+    // constructor
+    public Boat(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    // implement the methods from the interface
+    public String getVehicleName() {
+        return this.name;
+    }
+
+    public int getVehicleCapacity() {
+        return this.capacity;
+    }
 }
